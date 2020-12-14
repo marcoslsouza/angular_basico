19f8533c8f599cc32aaf9bd866bfdef0e30fd705
Marcos L. Souza <marcoslsouzadf@gmail.com>
2020-12-13 22:42:46 -0300

    Exemplos de criacao de components, modules, templates e foreach em listas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HelloComponent} from './hello/hello.component'
+import {HelloComponent} from './hello/hello.component';
+import { CursoAngularComponent } from './curso-angular/curso-angular.component'
+
+import { ClientesModule } from './clientes/clientes.module'
+
+import { HelloModule } from './hello/hello.module'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    CursoAngularComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClientesModule,
+    HelloModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,11 +1,25 @@
-import {Component} from '@angular/core'
+import { Component} from '@angular/core';
 
 @Component({
     selector: 'hello', 
-    template: `
-        <h1>Hello World</h1>
-    `
+    templateUrl: './hello.component.html'
 })
 export class HelloComponent {
 
+    //private nome : string;
+    public clientes : Cliente[];
+
+    constructor() {
+        //this.nome = 'Marcos';
+        let fulano = new Cliente('Maria', 20);
+        let cicrano = new Cliente('João', 22);
+        let outro = new Cliente('Marcos', 39);
+        this.clientes = [fulano, cicrano, outro];
+    }
+
+}
+
+class Cliente {
+    constructor(public nome : string, public idade : number) {
+    }
 }
